220363309e9b328d67f7ce1f0faaf98623ba8c6e
Poorjoy <madison_shepard@mail.ru>
2020-03-02 17:13:33 +0300

    feat: extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,16 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    var result;
+    options = {
+        repeatTimes: 'repeatTimes' in options ? options.repeatTimes : 1,
+        separator: 'separator' in options ? options.separator : '+',
+        addition: 'addition' in options ? options.addition : '',
+        additionRepeatTimes: 'additionRepeatTimes' in options ? options.additionRepeatTimes : 1,
+        additionSeparator: 'additionSeparator' in options ? options.additionSeparator : '|',
+    };
+    var addSep = options.addition + options.additionSeparator;
+    var addRep = addSep.repeat(options.additionRepeatTimes - 1) + options.addition;
+    var newStr = str + addRep + options.separator;
+    result = newStr.repeat(options.repeatTimes - 1) + str + addRep;
+    return result;
 };
   
